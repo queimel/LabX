97777c00ab2f2ec129e46d9790e957f47d564dd3
Cristian Campos Aviles <queimel@iMac-de-Cristian.local>
2019-10-12 21:02:27 -0300

    fix bugs

--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -97,7 +97,7 @@
 
                     <div class="form-group d-flex justify-content-end">
                         <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Editar Cliente</button>
-                        <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancelar</button>
+                        <a class="btn btn-inverse waves-effect waves-light" href="{{ URL::previous() }}">Cancelar</a>
                     </div>
                 </div>
             </div>
--- resources/views/admin/equipos/modelos/create.blade.php
+++ resources/views/admin/equipos/modelos/create.blade.php
@@ -61,7 +61,7 @@
                         <hr>
                         <div class="form-group d-flex justify-content-end">
                             <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Crear Modelo</button>
-                            <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancelar</button>
+                            <a class="btn btn-inverse waves-effect waves-light" href="{{ URL::previous() }}">Cancelar</a>
                         </div>
                     </div>
                 </div>
